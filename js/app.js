b4919b0defa03e29572c6927159db507b97a1337
Matt McCue <mhoward13@spu.edu>
2020-11-23 19:00:01 -0800

    Added JS for contact change

---
+++ js/app.js
@@ -0,0 +1,13 @@
+"use strict";
+
+
+var contacts = document.getElementsByClassName("contacts");
+
+var toggleInfo = function toggleInfo() {
+    var emails = document.querySelectorAll(".contact-email"), phones = document.querySelectorAll(".contact-phone"), i;
+    /*Toggle the hide class on each contact column*/
+    for (i = 0; i < emails.length; i++) {
+        emails[i].classList.toggle("hide");
+        phones[i].classList.toggle("hide");
+    }
+};
