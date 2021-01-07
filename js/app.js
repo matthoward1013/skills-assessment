6a4b068409688d34c4523377727e461b857d5297
Matt McCue <mhoward13@spu.edu>
2021-01-07 15:29:33 -0800

    Finally got the hover menu to work oof

--- js/app.js
+++ js/app.js
@@ -12,3 +12,37 @@ var toggleInfo = function toggleInfo() {
 };
 
 /*Code here would be for the dropdown menu*/
+
+function dropdownHover(current) {
+    var otherRows = document.getElementsByClassName("row");
+
+    var dropdownItems = current.getElementsByClassName("drop");
+    var i;
+    
+    for (i = 0; i < dropdownItems.length; i++) {
+        dropdownItems[i].classList.toggle("hide");
+    }
+
+    for (var i = 0; i < otherRows.length; i++) {
+        if (otherRows[i] === current) {
+            otherRows[i].style.opacity = "1";
+        } else {
+            otherRows[i].style.opacity = "0.1";
+        }
+    }
+}
+
+function dropdownLeave(current) {
+    var otherRows = document.getElementsByClassName("row");
+
+    var dropdownItems = current.getElementsByClassName("drop");
+    var i;
+    
+    for (i = 0; i < dropdownItems.length; i++) {
+        dropdownItems[i].classList.toggle("hide");
+    }
+
+    for (var i = 0; i < otherRows.length; i++) {
+        otherRows[i].style.opacity = "1";
+    }
+}
